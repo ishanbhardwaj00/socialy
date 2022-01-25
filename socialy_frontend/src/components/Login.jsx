3b83a627ab457550c2059248545a37028ba44f9a
ishanbhardwaj00 <ishan.x.bhardwaj@gmail.com>
2022-01-25 18:33:14 +0530

    google oauth added

--- socialy_frontend/src/components/Login.jsx
+++ socialy_frontend/src/components/Login.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons';
+import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4'
+import { client } from '../client';
+
 
 const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    console.log(response);
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+  
+    const { name, googleId, imageUrl } = response.profileObj;
+  
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl
+    };
+  
+    client.createIfNotExists(doc)
+      .then(() => {
+        navigate('/', { replace: true });
+      });
+  }
+
   return (
     <div className='flex justify-start items-center flex-row h-screen'>
       <div className='relative w-full h-full'>
@@ -18,9 +40,27 @@ const Login = () => {
           autoPlay
         />
       </div>
-      <div className='absolute flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay '>
-        <div className='p-5'>
-          
+      <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay z-50 '>
+        <div className='p-5 text-white font-mono'>
+            <h3>s o c i a l y</h3>
+        </div>
+        <div className='shadow-2xl'>
+          <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+            render={(renderProps) => (
+              <button
+                className="bg-mainColor flex object-center items-center px-4 py-2 rounded-lg text-sm font-light outline-none"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+              sign in with
+              <FcGoogle className='ml-2 text-xl' />
+              </button>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy='single_host_origin'
+          />
         </div>
       </div>
     </div>
--- socialy_frontend/src/containers/Home.jsx
+++ socialy_frontend/src/containers/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { HiMenu } from 'react-icons/ai'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { Link, Route, Routes } from 'react-router-dom'
+import { Sidebar, UserProfile  } from '../components'
+import Pins from './Pins'
+import { client } from '../client'
 
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex bg-gray-50'>
       <h1>Home</h1>
     </div>
   )
